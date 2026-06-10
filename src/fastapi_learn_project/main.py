b5628aea7ccacbe2e2c1b4e6087c2768ddb4e5f7
Gustavo Fontana Bertoldi <gustavobertoldi20@gmail.com>
2026-06-09 21:10:42 -0300

    creating another db models and updating the routs

--- src/fastapi_learn_project/main.py
+++ src/fastapi_learn_project/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from fastapi_learn_project.models.item_model import Item
+from fastapi_learn_project.models.item_model import Item, ItemPublic, CreateItem, ItemUpdate
 
 from contextlib import asynccontextmanager
 
@@ -17,25 +17,38 @@ app = FastAPI(lifespan=lifespan)
 App começa daqui pra baixo
 '''
 
-@app.post("/items/create_item")
-async def create_item(new_item:Item, session: SessionDep) -> Item:
+@app.post("/items/create_item", response_model=ItemPublic)
+async def create_item(item_in:CreateItem, session: SessionDep):
+    new_item = Item.model_validate(item_in)
     session.add(new_item)
     session.commit()
     session.refresh(new_item)
     return new_item
 
-@app.get("/items/{item_id}")
-async def get_item(id:int, session:SessionDep) -> Item:
+@app.get("/items/{item_id}", response_model=ItemPublic)
+async def get_item(id:int, session:SessionDep):
     item = session.get(Item, id)
     if not item:
         raise HTTPException(status_code=404, detail="item nao encontrado")
     return item
 
-@app.delete("/item/delete/{name}")
-async def delete_item(id:int, session:SessionDep) -> Item:
-    item = session.get(Item, id)
+@app.delete("/item/delete/{item_id}")
+async def delete_item(item_id:int, session:SessionDep):
+    item = session.get(Item, item_id)
     if not item:
         raise HTTPException(status_code=404, detail="item nao encontrado")
     session.delete(item)
     session.commit()
     return {"ok":True}
+
+@app.patch("/items/update/{item_id}", response_model=ItemPublic)
+async def update_item(item_id:int, item_in:ItemUpdate, session:SessionDep):
+    item_db = session.get(Item, item_id)
+    if not item_db:
+        raise HTTPException(status_code=404, detail="item nao encontrado")
+    item_data = item_in.model_dump(exclude_unset=True)
+    item_db.sqlmodel_update(item_data)
+    session.add(item_db)
+    session.commit()
+    session.refresh(item_db)
+    return item_db
--- src/fastapi_learn_project/models/item_model.py
+++ src/fastapi_learn_project/models/item_model.py
@@ -1,7 +1,20 @@
 from sqlmodel import Field, SQLModel
 
-class Item(SQLModel, table=True):
-    item_id:int | None = Field(default=None, primary_key=True)
+class ItemBase(SQLModel):
     name:str = Field(index=True)
     price:float | None = Field(default=None, index=True)
+
+class Item (ItemBase, table=True):
+    item_id:int | None = Field(default=None, primary_key=True)
+
+class ItemPublic(ItemBase):
+    item_id:int
+    description:str | None = None
+
+class CreateItem(ItemBase):
     description:str | None = Field(default=None)
+
+class ItemUpdate(ItemBase):
+    name:str | None = None
+    price:float | None = None
+    description:str | None = None
